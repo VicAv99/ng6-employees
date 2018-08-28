5785b2a556558e94aea89f8dd01d525ea17ad106
Victor Avila <victor.avila@venturplex.com>
2018-08-27 17:43:29 -0700

    delete works

--- client/app/components/employees/employeesList/employeesList.component.js
+++ client/app/components/employees/employeesList/employeesList.component.js
@@ -4,7 +4,8 @@ import './employeesList.scss';
 
 const bindings = {
   employees: '<',
-  selectedEmployee: '&'
+  selectedEmployee: '&',
+  deletedEmployee: '&'
 }
 
 let employeesListComponent = {

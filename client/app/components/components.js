23a56021f5491891b3f9a73baaef8379aee9804d
Victor Avila <victor.avila@venturplex.com>
2018-08-27 18:00:09 -0700

    build fixes and script

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,11 +1,9 @@
 import angular from 'angular';
 import Home from './home/home';
-import About from './about/about';
 import Employees from './employees/employees';
 
 let componentModule = angular.module('app.components', [
   Home,
-  About,
   Employees
 ])
 
